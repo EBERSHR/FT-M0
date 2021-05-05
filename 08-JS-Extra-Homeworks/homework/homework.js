337bf5b7482b0d22fd6f8e93281fd293424403d9
Eber Hernández <ebershr@gmail.com>
2021-05-05 16:16:57 -0500

    Homework 02-JS-I

--- 08-JS-Extra-Homeworks/homework/homework.js
+++ 08-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,11 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+  matriz = [];
+  for (var x in objeto) {
+    console.log(x);
+  }
+  return matriz;
 }
 
 
